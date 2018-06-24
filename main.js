3aae9087827579c163c52dd29b3a9d4806967bbd
Agus Alexander <agusalexander8@gmail.com>
2018-06-24 20:47:08 -0300

    added on the fly json obj loading methods

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+loadJSON(function(response){
+  var jsonInput = JSON.parse(response);
+  console.log(jsonInput);
+  if (IsJsonString(jsonInput)) {
+    toDOM(JSON.parse(jsonInput));
+  } else {
+    console.log("oops not valid json");
+  }
+  // Component to change to a sequential color on click.
+});
+
+AFRAME.registerComponent('cursor-listener', {
+  init: function () {
+    var lastIndex = -1;
+    var COLORS = ['red', 'green', 'lightblue'];
+    this.el.addEventListener('click', function (evt) {
+      lastIndex = (lastIndex + 1) % COLORS.length;
+      this.setAttribute('material', 'color', COLORS[lastIndex]);
+      console.log(this.getAttribute('scale'))
+      console.log('I was clicked at: ', evt.detail.intersection.point);
+      var el = document.createElement('a-entity');
+      document.querySelector('a-scene').appendChild(el);
+    });
+  }
+});
+
+
 
 function loadJSON(callback) {
 
@@ -12,20 +39,42 @@ function loadJSON(callback) {
    };
    xobj.send(null);
 }
-loadJSON(function(response){
-  var actual_JSON = JSON.parse(response);
-  console.log(actual_JSON);
-  // Component to change to a sequential color on click.
-});
-AFRAME.registerComponent('cursor-listener', {
-  init: function () {
-    var lastIndex = -1;
-    var COLORS = ['red', 'green', 'lightblue'];
-    this.el.addEventListener('click', function (evt) {
-      lastIndex = (lastIndex + 1) % COLORS.length;
-      this.setAttribute('material', 'color', COLORS[lastIndex]);
-      console.log(this.getAttribute('scale'))
-      console.log('I was clicked at: ', evt.detail.intersection.point);
-    });
-  }
-});
+
+
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+
+function appendObject(id, file, scale, position, rotation, scale) {
+     console.log(position);
+     $('<a-entity />', {
+       id: id,
+       class: 'city object children',
+       position: position,  // doesn't seem to do anything
+       scale: scale,
+       rotation: rotation,
+       file: file,
+       "obj-model": "obj: url(assets/obj/" + file + ".obj); mtl: url(assets/obj/" + file + ".mtl)",
+       // src: '#' + file + '-obj',
+       // mtl: '#' + file + '-mtl',
+       appendTo : $('#city')
+     });
+    document.getElementById(id).setAttribute("position", position); // workaround - this does set position
+}
+
+function toDOM(jsonInput) {
+ // this assumes it is being run first -- it will start placing objects with id 0 and set objectcount to the number of loaded objects
+ for(var i = 0; i < jsonInput.length; i++) {
+     var obj = jsonInput[i];
+     console.log("Adding entity #" + obj.id);
+     appendObject(obj.id, obj.file, obj.scale, obj.position, obj.rotation, obj.scale)
+ }
+ objectCount = i;
+ console.log("objectCount:" + objectCount);
+}
