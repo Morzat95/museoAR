d66eed168a7484f86b744c7ed89a021ae17acacc
Agus Alexander <agusalexander8@gmail.com>
2018-06-24 18:56:01 -0300

    fixed script and added https server script

--- virtualCamera.sh
+++ virtualCamera.sh
@@ -2,6 +2,8 @@ sudo apt-get -y install ffmpeg v4l-utils;
 git clone https://github.com/umlaeute/v4l2loopback.git;
 cd v4l2loopback;
 sudo make && make install;
+cp v4l2loopback.ko /lib/modules/`uname -r`
+sudo depmod -a
 sudo modprobe v4l2loopback;
 cd ..;
 ffmpeg -loop 1 -re -i kanji-marker.png -f v4l2 -vcodec rawvideo -pix_fmt yuv420p /dev/video1;
