8f5ea7d605972c1dfdbf5f63bd4f572669976765
Agus Alexander <agusalexander8@gmail.com>
2018-06-29 22:49:44 -0300

    overlay now works on mobile

--- bashScripts/runVirtualCamera.sh
+++ bashScripts/runVirtualCamera.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo depmod -a;
 sudo modprobe v4l2loopback;
 ffmpeg -loop 1 -re -i kanji-marker.png -f v4l2 -vcodec rawvideo -pix_fmt yuv420p /dev/video1;
--- main.js
+++ main.js
@@ -1,17 +1,73 @@
-loadJSON(function(response){
-  var jsonInput = JSON.parse(response);
-  console.log(jsonInput);
-  if (IsJsonString(jsonInput)) {
-    toDOM(JSON.parse(jsonInput));
-  } else {
-    console.log("oops not valid json");
-  }
-  // Component to change to a sequential color on click.
-});
+var jsonObjTerminals;
 
-function hide() {
-      $("#dialog").removeClass('intro-overlay');
-  }
+function showOrHideMenu() {
+    $("#menu").toggle();
+}
+
+function load() {
+    $("#menu").toggle();
+    jsonInput = $("#jsonInput")[0].value;
+    loadJSON(function (response){
+      console.log(response);
+      if (IsJsonString(response)) {
+        toDOM(JSON.parse(response));
+      } else {
+        console.log("oops not valid json");
+      }
+      // Component to change to a sequential color on click.
+    },jsonInput);
+}
+
+
+function loadJSON(callback,filename) {
+
+   var xobj = new XMLHttpRequest();
+       xobj.overrideMimeType("application/json");
+   xobj.open('GET', filename, true); // Replace 'my_data' with the path to your file
+   xobj.onreadystatechange = function () {
+         if (xobj.readyState == 4 && xobj.status == "200") {
+           // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+           callback(xobj.responseText);
+         }
+   };
+   xobj.send(null);
+}
+
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+function toDOM(jsonInput) {
+ // this assumes it is being run first -- it will start placing objects with id 0 and set objectcount to the number of loaded objects
+ for(var i = 0; i < jsonInput.length; i++) {
+     var obj = jsonInput[i];
+     console.log("Adding entity #" + obj.id);
+     appendObject(obj.id, obj.file, obj.scale, obj.position, obj.rotation, obj.material)
+ }
+ objectCount = i;
+ console.log("objectCount:" + objectCount);
+}
+
+function appendObject(id, file, scale, position, rotation, scale) {
+     console.log(position);
+     $('<a-entity />', {
+       id: id,
+       class: 'object children',
+       position: position,  // doesn't seem to do anything
+       scale: scale,
+       rotation: rotation,
+       file: file,
+       "obj-model": "obj: url(assets/objects/" + file + ".obj); mtl: url(assets/objects/" + file + ".mtl)",
+       appendTo : $('#marker')
+     });
+    document.getElementById(id).setAttribute("position", position); // workaround - this does set position
+}
+
 
 AFRAME.registerComponent('cursor-listener', {
   init: function () {
@@ -27,58 +83,3 @@ AFRAME.registerComponent('cursor-listener', {
     });
   }
 });
-
-
-
-function loadJSON(callback) {
-
-   var xobj = new XMLHttpRequest();
-       xobj.overrideMimeType("application/json");
-   xobj.open('GET', 'models/trafo4Bornes/trafo4Bornes.json', true); // Replace 'my_data' with the path to your file
-   xobj.onreadystatechange = function () {
-         if (xobj.readyState == 4 && xobj.status == "200") {
-           // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-           callback(xobj.responseText);
-         }
-   };
-   xobj.send(null);
-}
-
-
-function IsJsonString(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
-
-
-function appendObject(id, file, scale, position, rotation, scale) {
-     console.log(position);
-     $('<a-entity />', {
-       id: id,
-       class: 'city object children',
-       position: position,  // doesn't seem to do anything
-       scale: scale,
-       rotation: rotation,
-       file: file,
-       "obj-model": "obj: url(assets/obj/" + file + ".obj); mtl: url(assets/obj/" + file + ".mtl)",
-       // src: '#' + file + '-obj',
-       // mtl: '#' + file + '-mtl',
-       appendTo : $('#city')
-     });
-    document.getElementById(id).setAttribute("position", position); // workaround - this does set position
-}
-
-function toDOM(jsonInput) {
- // this assumes it is being run first -- it will start placing objects with id 0 and set objectcount to the number of loaded objects
- for(var i = 0; i < jsonInput.length; i++) {
-     var obj = jsonInput[i];
-     console.log("Adding entity #" + obj.id);
-     appendObject(obj.id, obj.file, obj.scale, obj.position, obj.rotation, obj.scale)
- }
- objectCount = i;
- console.log("objectCount:" + objectCount);
-}
