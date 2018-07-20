1519066621cda3abacb5f4be838b228260b958ef
Agus Alexander <agusalexander8@gmail.com>
2018-07-20 00:29:25 -0300

    added click to items and more interactions

--- arController.js
+++ arController.js
@@ -3,35 +3,66 @@ var name = a.substring(a.indexOf("=")+1);
 var activity;
 var levelsFile;
 var playing=false;
+var overridenLevel="";
+var currentLevel=0;
 
 function run(){
     console.log("activity= "+name);
     loadJSON("assets/"+name+".lvl.json",loadLevels);
 }
 
+function levelWasOverriden(level){
+  if(level==overridenLevel){
+    return true;
+  }
+  return false;
+}
 
 function loadLevels(jsonInput){
-  loadLevel(jsonInput,0);
+  activity=jsonInput;
+  loadLevel(jsonInput);
 
 }
-function loadLevel(jsonInput,index){
-  console.log("iteracion: "+index);
-
-  var item=jsonInput[index];
-  console.log(item);
+function goTo(level){
+  overridenLevel=currentLevel;
+  removeAllChildren(activity[currentLevel].marker);
+  currentLevel=level;
+  loadLevel(activity);
+ 
+}
+function stop(id){
+  console.log("stop the video!");
+  var vid = document.getElementById(id);
+  vid.stop();
+}
+function loadLevel(jsonInput){
+  console.log("Nivel: "+currentLevel);
+  var item=jsonInput[currentLevel];
   appendText(item.description);
-  toDOM(item.objects,'#marker');
+  toDOM(item.objects,item.marker);
+  if(item.type=="delay"){
+      var delayInMilliseconds = item.delay;
+      setTimeout(function() {
+        if(!levelWasOverriden(item.id)){
+          if(currentLevel<jsonInput.length-1){
+            console.log("removing!!");
+            removeAllChildren(item.marker);
+            currentLevel=currentLevel+1;
+            loadLevel(jsonInput);
+          }
+          else{
+            console.log("Activity Finished");
+          }
+      }
+      else{
+        overridenLevel="";
+      }
 
-  var delayInMilliseconds = item.delay;
-  setTimeout(function() {
-    if(index<jsonInput.length-1){
-      removeAllChildren('#marker');
-      loadLevel(jsonInput,index+1);
-    }
-    else{
-      console.log("Activity Finished");
-    }
       }, delayInMilliseconds);
+    }
+
+    
+
 }
 
 function toDOM(jsonInput,father) {
@@ -47,7 +78,7 @@ function toDOM(jsonInput,father) {
       console.log("objectCount:" + objectCount);
 }
 function removeAllChildren(father){
-  var marker= document.querySelector(father);
+  var marker= document.querySelector('#'+father);
   marker.removeChild(marker.childNodes[0]);
 }
   
@@ -61,46 +92,55 @@ function appendText(text){
 }
 
 function appendObject(jObj,father){
-      var marker= document.querySelector(father);
-      var obj = document.createElement('a-entity');
+      var marker= document.querySelector('#'+father);
+      var obj = document.createElement(jObj.type);
       obj.setAttribute('id',jObj.id); 
-
       obj.setAttribute('obj-model','obj','url('+jObj.file+')');
       obj.setAttribute('scale',jObj.scale);
       obj.setAttribute('rotation',jObj.rotation);
       obj.setAttribute('position',jObj.position);
-      obj.setAttribute('material','color',jObj.material.color);
       obj.setAttribute('isClicked',false);
-      if(jObj.cursorListener==true){
+      obj.setAttribute('text','value'," Hello World");
+      if(jObj.onclick!=null){
         obj.setAttribute('cursor-listener','');
+        obj.setAttribute('onclick',jObj.onclick);
       }
+      if(jObj.material!=null){
+        obj.setAttribute('material','color',jObj.material.color);
+      }
+      else{
+        obj.setAttribute('src',jObj.src);
+      }   
       marker.appendChild(obj);
       console.log("Adding entity #" + jObj.id);
-      console.log(jObj.material.color);
       console.log(marker.querySelector("#" +jObj.id));
-      toDOM(jObj.children,"#"+jObj.id);
-      
-      
+      toDOM(jObj.children,jObj.id);
+     
 }
   
 AFRAME.registerComponent('cursor-listener', {
   init: function () {
-    var lastIndex = -1;
-    var COLORS = ['red', 'green', 'lightblue'];
     this.el.addEventListener('click', function (evt) {
-      if(this.getAttribute('isClicked')=="true"){
+     /* if(this.getAttribute('isClicked')=="true"){
         this.setAttribute('material', {color: 'grey'});
         this.setAttribute('isClicked',false);
+
       }
       else{
         this.setAttribute('material', {color: 'blue'});
         this.setAttribute('isClicked',true);
-      }
+      }*/
       console.log("#" +this.id+" was clicked");
-      console.log('I was clicked at: ', evt.detail.intersection.point);
+     // console.log('I was clicked at: ', evt.detail.intersection.point);
+      this.onclick;
+
     });
   }
 });
+
+function isMarkerVisible(marker){
+    return document.querySelector("#"+marker).object3D.visible;
+}
 
 AFRAME.registerComponent('markerhandler', {
 	init: function() {
@@ -108,17 +148,19 @@ AFRAME.registerComponent('markerhandler', {
 		this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
 	},
 	tick: function(t, dt) {
-    console.log("searching marker...");
-		
-		if (document.querySelector("#marker").object3D.visible == true && playing == false) {
+		if (isMarkerVisible('marker') && playing == false) {
       // MARKER IS PRESENT
       console.log("Found!");
       playing = true;
       run();
-      document.querySelector("#marker").removeAttribute("markerhandler");
-		} else {
-			// MARKER IS HIDDEN, do nothing (up to you)
-		}
+      
+    } else if ((isMarkerVisible('marker')==false) && (playing == true)) {
+     
+			console.log("marker lost!");
+    }
+    else if((isMarkerVisible('marker')==false) && (playing == false)){
+      console.log("looking for marker...");
+    }
 
 	}
 });
