9187ba7d1888566341004a45b1b9404fc766cafb
Agus Alexander <agusalexander8@gmail.com>
2018-07-20 16:54:47 -0300

    major fixes

--- arController.js
+++ arController.js
@@ -1,75 +1,60 @@
 var a = window.location.toString();
 var name = a.substring(a.indexOf("=")+1);
 var activity;
-var levelsFile;
 var playing=false;
-var overridenLevel="";
-var currentLevel=0;
+var currentItem=0;
 
 function run(){
     console.log("activity= "+name);
-    loadJSON("assets/"+name+".lvl.json",loadLevels);
+    loadJSON("assets/"+name+".item.json",loadItems);
 }
 
-function levelWasOverriden(level){
-  if(level==overridenLevel){
-    return true;
-  }
-  return false;
-}
 
-function loadLevels(jsonInput){
+function loadItems(jsonInput){
   activity=jsonInput;
-  loadLevel(jsonInput);
+  currentItem=activity[0];
+}
 
+function goTo(index){
+  currentTimeout="";
+  if(currentItem.persistent==null){
+    removeAllChildren(currentItem.marker);
+  }
+  currentItem=activity[index];
+  playing=false;
 }
-function goTo(level){
-  overridenLevel=currentLevel;
-  removeAllChildren(activity[currentLevel].marker);
-  currentLevel=level;
-  loadLevel(activity);
- 
-}
-function stop(id){
-  console.log("stop the video!");
-  var vid = document.getElementById(id);
-  vid.stop();
-}
-function loadLevel(jsonInput){
-  console.log("Nivel: "+currentLevel);
-  var item=jsonInput[currentLevel];
-  appendText(item.description);
+
+
+function loadItem(item){
+  showText(item.description);
   toDOM(item.objects,item.marker);
   if(item.type=="delay"){
-      var delayInMilliseconds = item.delay;
-      setTimeout(function() {
-        if(!levelWasOverriden(item.id)){
-          if(currentLevel<jsonInput.length-1){
-            console.log("removing!!");
-            removeAllChildren(item.marker);
-            currentLevel=currentLevel+1;
-            loadLevel(jsonInput);
-          }
-          else{
-            console.log("Activity Finished");
-          }
-      }
-      else{
-        overridenLevel="";
-      }
-
-      }, delayInMilliseconds);
+      startTimer(item);
     }
+  if(currentItem.type=="redirect"){
+    window.location.href = item.destiny;
+  }
+}
 
-    
+function startTimer(item){
+  var delayInMilliseconds = item.delay;
+  setTimeout(function(timeout) {
+        if(playing){ //if marker is still visible
+          if(item.persistent==null){
+            removeAllChildren(item.marker);}
+          currentItem=activity[item.next];
+          playing=false;
+          console.log("next! "+item.next);
+                  }
+  }, delayInMilliseconds);
 
 }
+
 
 function toDOM(jsonInput,father) {
     if(jsonInput==null ||jsonInput=="")
     {return;
     }
-      // this assumes it is being run first -- it will start placing objects with id 0 and set objectcount to the number of loaded objects
       for(var i = 0; i < jsonInput.length; i++) {
           var obj = jsonInput[i];
           appendObject(obj,father);
@@ -77,18 +62,25 @@ function toDOM(jsonInput,father) {
       objectCount = i;
       console.log("objectCount:" + objectCount);
 }
+
 function removeAllChildren(father){
+  
   var marker= document.querySelector('#'+father);
-  marker.removeChild(marker.childNodes[0]);
+  if(marker!=null){
+    while (marker.firstChild) {
+      marker.removeChild(marker.firstChild);
+      }
+    }
 }
   
-function appendText(text){
+function showText(text){
   var obj = document.createElement('li');
   obj.innerText=text;
   var list= document.querySelector("#checklist");
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+    }
   list.appendChild(obj);
-
-
 }
 
 function appendObject(jObj,father){
@@ -100,13 +92,13 @@ function appendObject(jObj,father){
       obj.setAttribute('rotation',jObj.rotation);
       obj.setAttribute('position',jObj.position);
       obj.setAttribute('isClicked',false);
-      obj.setAttribute('text','value'," Hello World");
       if(jObj.onclick!=null){
         obj.setAttribute('cursor-listener','');
         obj.setAttribute('onclick',jObj.onclick);
       }
       if(jObj.material!=null){
-        obj.setAttribute('material','color',jObj.material.color);
+        obj.setAttribute('material',jObj.material);
+
       }
       else{
         obj.setAttribute('src',jObj.src);
@@ -118,49 +110,57 @@ function appendObject(jObj,father){
      
 }
   
+
+function isCurrentMarkerVisible(){
+    if(currentItem==null){
+      return false;
+    }
+    if(currentItem.marker==null){//if it has no marker its excecuted anyway
+      return true;
+    }
+    return document.querySelector("#"+currentItem.marker).object3D.visible;
+}
+
+
+AFRAME.registerComponent('markerhandler', {
+  init: function() {
+    // Set up the tick throttling. Will check if marker is active every 500ms
+    console.log("setting up marker handler...");
+    this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
+  },
+  tick: function(t, dt) {
+    if(activity!=null){
+        if (isCurrentMarkerVisible() && playing == false) {
+          // MARKER IS PRESENT
+          console.log("Found!");
+          currentTimeout="";
+          playing = true;
+          loadItem(currentItem);
+          
+        } else if ((isCurrentMarkerVisible()==false) && (playing == true)) {
+          currentTimeout="";
+          removeAllChildren(currentItem.marker);
+          playing=false;
+          console.log("marker lost!");
+        }
+        else if((isCurrentMarkerVisible()==false) && (playing == false)){
+          if(activity!=null){//not yet loaded
+              showText("Buscando el marker: "+currentItem.marker);
+              console.log("looking for marker... "+currentItem.marker);
+          }
+        }
+
+      }
+    }
+});
+
 AFRAME.registerComponent('cursor-listener', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
-     /* if(this.getAttribute('isClicked')=="true"){
-        this.setAttribute('material', {color: 'grey'});
-        this.setAttribute('isClicked',false);
-
-      }
-      else{
-        this.setAttribute('material', {color: 'blue'});
-        this.setAttribute('isClicked',true);
-      }*/
       console.log("#" +this.id+" was clicked");
-     // console.log('I was clicked at: ', evt.detail.intersection.point);
       this.onclick;
-
     });
   }
 });
 
-function isMarkerVisible(marker){
-    return document.querySelector("#"+marker).object3D.visible;
-}
 
-AFRAME.registerComponent('markerhandler', {
-	init: function() {
-		// Set up the tick throttling. Will check if marker is active every 500ms
-		this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
-	},
-	tick: function(t, dt) {
-		if (isMarkerVisible('marker') && playing == false) {
-      // MARKER IS PRESENT
-      console.log("Found!");
-      playing = true;
-      run();
-      
-    } else if ((isMarkerVisible('marker')==false) && (playing == true)) {
-     
-			console.log("marker lost!");
-    }
-    else if((isMarkerVisible('marker')==false) && (playing == false)){
-      console.log("looking for marker...");
-    }
-
-	}
-});
