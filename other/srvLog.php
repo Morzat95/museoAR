77ebe0df238c87a45538c39714936ae4d718fdb0
Federico <fe_de_qui9@hotmail.com>
2019-09-10 06:07:42 -0300

    Adding timestamp to log

--- other/srvLog.php
+++ other/srvLog.php
@@ -6,9 +6,10 @@
         $activityID64 = (isset($_POST['name']) ? $_POST['name'] : "");
         $action64 = (isset($_POST['action']) ? $_POST['action'] : "");
         $value64 = (isset($_POST['value']) ? $_POST['value'] : "");
+        $timestamp64 = (isset($_POST['timestamp']) ? $_POST['timestamp'] : "");
         $id64 = (isset($_POST['id']) ? $_POST['id'] : "");
 
-        if($activityID64 === "" || $action64 === "" || $value64 === "" || $id64 === "")
+        if($activityID64 === "" || $action64 === "" || $value64 === "" || $timestamp64 === "" ||$id64 === "")
         {
             throw new Exception("Request error!");
         }
@@ -17,11 +18,12 @@
         $activityID = base64_decode($activityID64);
         $action = base64_decode($action64);
         $value = base64_decode($value64);
+        $timestamp = base64_decode($timestamp64);
         $id = base64_decode($id64);
 
         // format
-        $header = "ActivityID,Action,Value,Id\n";
-        $row = "$activityID,$action,$value,$id\n";
+        $header = "ActivityID,Action,Value,Timestamp,Id\n";
+        $row = "$activityID,$action,$value,$timestamp,$id\n";
 
         if(!file_exists("log.csv")){
             file_put_contents("log.csv", $header);
